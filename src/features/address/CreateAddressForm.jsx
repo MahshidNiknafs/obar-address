a9fb44ae649dad8dbba97303ee4553412976e3ac
Mahshid Niknafs <mahshidniknafs.asd@gmail.com>
2025-02-09 10:30:04 +0330

    add form styling

--- src/features/address/CreateAddressForm.jsx
+++ src/features/address/CreateAddressForm.jsx
@@ -25,8 +25,27 @@ function CreateAddressForm() {
 					/>
 				</div>
 
+				{/* <div className="input-container">
+					<label className="label">
+						شماره تلفن ثابت
+						<span className="optional"> (اختیاری)</span>
+						<span className="note">*با پیش شماره</span>
+					</label>
+					<input
+						className="input"
+						type="number"
+						placeholder="مثال: ۰۲۱۱۲۳۴۵۶۷"
+					/>
+				</div> */}
+
 				<div className="input-container">
-					<label className="label">شماره تلفن ثابت (اختیاری)</label>
+					<div className="label-container">
+						<label className="label">
+							شماره تلفن ثابت
+							<span className="optional"> (اختیاری)</span>
+						</label>
+						<span className="note">*با پیش شماره</span>
+					</div>
 					<input
 						className="input"
 						type="number"
@@ -63,118 +82,3 @@ function CreateAddressForm() {
 }
 
 export default CreateAddressForm;
-//////////////////////////////////////////////////////
-// function CreateAddressForm() {
-// 	return <div style={{ backgroundColor: "red" }}>CreateAddressForm</div>;
-// }
-
-// export default CreateAddressForm;
-
-/////////////////////////////
-// import "./CreateAddressForm.css";
-
-// function CreateAddressForm() {
-// 	return (
-// 		<form className="form-container">
-// 			<h1 className="form-title">لطفا مشخصات و آدرس خود را وارد کنید</h1>
-
-// 			<div className="form-inputs">
-// 				<div className="input-container">
-// 					<label className="label">نام</label>
-// 					<input className="input" type="text" placeholder="نام" />
-// 				</div>
-
-// 				<div className="input-container">
-// 					<label className="label">نام خانوادگی</label>
-// 					<input className="input" type="text" placeholder="نام خانوادگی" />
-// 				</div>
-
-// 				<div className="input-container">
-// 					<label className="label">شماره تلفن همراه</label>
-// 					<input
-// 						className="input"
-// 						type="number"
-// 						placeholder="شماره تلفن همراه"
-// 					/>
-// 				</div>
-
-// 				<div className="input-container">
-// 					<label className="label">شماره تلفن ثابت (اختیاری)</label>
-// 					<input
-// 						className="input"
-// 						type="number"
-// 						placeholder="شماره تلفن ثابت"
-// 					/>
-// 				</div>
-
-// 				<div className="textarea-container">
-// 					<label className="label">آدرس</label>
-// 					<textarea
-// 						className="textarea"
-// 						placeholder="آدرس خود را وارد کنید"
-// 					></textarea>
-// 				</div>
-// 			</div>
-
-// 			<div className="gender-container">
-// 				<label className="label">جنسیت</label>
-// 				<div className="radio-group">
-// 					<label className="radio-label">
-// 						<input className="radio" type="radio" name="gender" value="male" />
-// 						<span>مرد</span>
-// 					</label>
-// 					<label className="radio-label">
-// 						<input
-// 							className="radio"
-// 							type="radio"
-// 							name="gender"
-// 							value="female"
-// 						/>
-// 						<span>زن</span>
-// 					</label>
-// 				</div>
-// 			</div>
-// 		</form>
-// 	);
-// }
-
-// export default CreateAddressForm;
-//////////////////////////////////////////
-// import "./CreateAddressForm.css";
-
-// function CreateAddressForm() {
-// 	return (
-// 		<form className="form-container">
-// 			<h1 className="form-title">لطفا مشخصات و آدرس خود را وارد کنید</h1>
-
-// 			<div>
-// 				<div className="input-container">
-// 					<label className="label">نام</label>
-// 					<input className="input" type="text" />
-// 				</div>
-
-// 				<div className="input-container">
-// 					<label className="label">نام خانوادگی</label>
-// 					<input className="input" type="text" />
-// 				</div>
-
-// 				<div className="input-container">
-// 					<label className="label">شماره تلفن همراه</label>
-// 					<input className="input" type="number" />
-// 				</div>
-
-// 				<div className="input-container">
-// 					<label className="label">شماره تلفن ثابت (اختیاری)</label>
-// 					<input className="input" type="number" />
-// 				</div>
-
-// 				<div className="">
-// 					<label className="label">آدرس</label>
-// 					<textarea />
-// 				</div>
-// 			</div>
-// 		</form>
-// 	);
-// }
-
-// export default CreateAddressForm;
