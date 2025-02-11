499e1d95306e79ba9628ceec52d20e1bfa47c64e
Mahshid Niknafs <mahshidniknafs.asd@gmail.com>
2025-02-11 22:40:23 +0330

    fix style bug

--- src/pages/CreateAddressPage.jsx
+++ src/pages/CreateAddressPage.jsx
@@ -2,11 +2,11 @@ import CreateAddressForm from "../features/address/CreateAddressForm";
 import Card from "../ui/Card/Card";
 
 function CreateAddressPage() {
-	return (
-		<Card cardTitle="ثبت آدرس">
-			<CreateAddressForm />
-		</Card>
-	);
+  return (
+    <Card cardTitle="ثبت آدرس">
+      <CreateAddressForm />
+    </Card>
+  );
 }
 
 export default CreateAddressPage;
--- src/ui/Footer/Footer.jsx
+++ src/ui/Footer/Footer.jsx
@@ -4,26 +4,26 @@ import { useFormContext } from "../../contexts/FormContext";
 import "./Footer.css";
 
 function Footer() {
-	const { submitAction } = useFormContext();
-	const navigate = useNavigate();
+  const { submitAction } = useFormContext();
+  const navigate = useNavigate();
 
-	const handleClick = () => {
-		if (submitAction) {
-			submitAction();
-			navigate("/create-address/set-location");
-		} else {
-			console.warn("No submit action registered.");
-		}
-	};
+  const handleClick = () => {
+    if (submitAction) {
+      submitAction();
+      //   navigate("/create-address/set-location");
+    } else {
+      console.warn("No submit action registered.");
+    }
+  };
 
-	const loading = false;
-	const disabled = false;
+  const loading = false;
+  const disabled = false;
 
-	return (
-		<footer className="layout-footer">
-			<Button onClick={handleClick} loading={loading} disabled={disabled} />
-		</footer>
-	);
+  return (
+    <footer className="layout-footer">
+      <Button onClick={handleClick} loading={loading} disabled={disabled} />
+    </footer>
+  );
 }
 
 export default Footer;
