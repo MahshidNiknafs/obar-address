9f43f24cae9af2f84105c358c8a85db6e6d5abeb
Mahshid Niknafs <mahshidniknafs.asd@gmail.com>
2025-02-11 15:47:16 +0330

    add addresses

--- src/pages/AddressesPage.jsx
+++ src/pages/AddressesPage.jsx
@@ -1,5 +1,12 @@
+import Addresses from "../features/address/Addresses";
+import Card from "../ui/Card/Card";
+
 function AddressesPage() {
-	return <>AddressesPage</>;
+	return (
+		<Card cardTitle="آدرس ها و مشخصات">
+			<Addresses />
+		</Card>
+	);
 }
 
 export default AddressesPage;
