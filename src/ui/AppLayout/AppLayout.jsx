12399e31221669352d5d61a264278aa148c02416
Mahshid Niknafs <mahshidniknafs.asd@gmail.com>
2025-02-12 13:11:16 +0330

    add conditional footer

--- src/ui/AppLayout/AppLayout.jsx
+++ src/ui/AppLayout/AppLayout.jsx
@@ -1,3 +1,4 @@
+import { useLocation } from "react-router";
 import Header from "../Header/Header";
 import Main from "../Main/Main";
 import Footer from "../Footer/Footer";
@@ -5,11 +6,14 @@ import Footer from "../Footer/Footer";
 import "./AppLayout.css";
 
 function AppLayout() {
+	const location = useLocation();
+	const showFooter = location.pathname.includes("create-address");
+
 	return (
 		<div className="layout">
 			<Header />
 			<Main />
-			<Footer />
+			{showFooter && <Footer />}
 		</div>
 	);
 }
